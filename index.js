09b6a8609e234048cc0bd09f9b578f00138b5125
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-11-01 19:12:33 +0530

    fix: generate spacing between object entries

--- index.js
+++ index.js
@@ -6,20 +6,20 @@ const transformVendorKeys = require('./transformVendorKeys');
 
 exports.chrome = input => {
 	const manifest = transformVendorKeys(input, 'chrome');
-	return { name: 'manifest.json', content: JSON.stringify(manifest) };
+	return { name: 'manifest.json', content: JSON.stringify(manifest, null, 2) };
 };
 
 exports.firefox = input => {
 	const manifest = transformVendorKeys(input, 'firefox');
-	return { name: 'manifest.json', content: JSON.stringify(manifest) };
+	return { name: 'manifest.json', content: JSON.stringify(manifest, null, 2) };
 };
 
 exports.opera = input => {
 	const manifest = transformVendorKeys(input, 'opera');
-	return { name: 'manifest.json', content: JSON.stringify(manifest) };
+	return { name: 'manifest.json', content: JSON.stringify(manifest, null, 2) };
 };
 
 exports.edge = input => {
 	const manifest = transformVendorKeys(input, 'edge');
-	return { name: 'manifest.json', content: JSON.stringify(manifest) };
+	return { name: 'manifest.json', content: JSON.stringify(manifest, null, 2) };
 };
