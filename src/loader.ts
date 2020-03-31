8bb2843ed3dfaf4ebb81815ce27daa87cbdb4517
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2020-04-01 01:19:38 +0530

    feat: make package as a webpack loader (WIP)

--- src/loader.ts
+++ src/loader.ts
@@ -1,12 +1,12 @@
-const browserVendors = ['chrome', 'firefox', 'opera', 'edge'];
+const browserVendors: string[] = ['chrome', 'firefox', 'opera', 'edge'];
 const vendorRegExp = new RegExp(`^__((?:(?:${browserVendors.join('|')})\\|?)+)__(.*)`);
 
 /**
  *  Fork of `webextension-toolbox/webpack-webextension-plugin`
  */
-const transformVendorKeys = (manifest, vendor) => {
+const transformVendorKeys = (manifest, vendor: string) => {
 	if (Array.isArray(manifest)) {
-		return manifest.map(newManifest => {
+		return manifest.map((newManifest) => {
 			return transformVendorKeys(newManifest, vendor);
 		});
 	}
@@ -16,7 +16,7 @@ const transformVendorKeys = (manifest, vendor) => {
 			const match = key.match(vendorRegExp);
 
 			if (match) {
-				const vendors = match[1].split('|');
+				const vendors: string[] = match[1].split('|');
 
 				// Swap key with non prefixed name
 				if (vendors.indexOf(vendor) > -1) {
@@ -32,4 +32,14 @@ const transformVendorKeys = (manifest, vendor) => {
 
 	return manifest;
 };
-module.exports = transformVendorKeys;
+
+export default function (content): string {
+	// eslint-disable-next-line no-unused-expressions
+	this.cacheable && this.cacheable();
+
+	const manifest = typeof content === 'string' ? JSON.parse(content) : content;
+	// ToDo: get vendor name from env TARGET_BROWSER
+	const transform = transformVendorKeys(manifest, 'firefox');
+
+	return `${transform}`;
+}
