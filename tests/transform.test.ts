5b080e8eb65b06b919a614ae2fc65a92733e88c8
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2022-06-12 23:55:41 +0530

    chore: more tests

--- tests/transform.test.ts
+++ tests/transform.test.ts
@@ -6,6 +6,33 @@ describe('transformManifest tests', () => {
     expect(transformManifest({}, 'chrome')).toEqual({});
   });
 
+  it('should return correct JSON for JSON without vendor prefixes', () => {
+    expect(
+      transformManifest(
+        JSON.parse(`
+			{
+			  "name": "test",
+			  "manifest_version": 2
+			}
+        `),
+        Browser.CHROME
+      )
+    ).toEqual(
+      JSON.parse(`
+			{
+			  "name": "test",
+			  "manifest_version": 2
+			}
+      `)
+    );
+  });
+});
+
+describe('Vendor Tests', () => {
+  //
+});
+
+describe('chrome tests', () => {
   it('should return correct JSON for chrome', () => {
     expect(
       transformManifest(
@@ -21,6 +48,34 @@ describe('transformManifest tests', () => {
       JSON.parse(`
 			{
 			  "manifest_version": 3
+			}
+      `)
+    );
+  });
+
+  it('nested vendor keys', () => {
+    expect(
+      transformManifest(
+        JSON.parse(`
+			{
+			 "options_ui": {
+				"page": "options.html",
+				"open_in_tab": true,
+				"__chrome__chrome_style": false,
+				"__firefox|opera__browser_style": false
+			  }
+			}
+        `),
+        Browser.CHROME
+      )
+    ).toEqual(
+      JSON.parse(`
+			{
+			 "options_ui": {
+				"page": "options.html",
+				"open_in_tab": true,
+				"chrome_style": false
+			  }
 			}
       `)
     );
@@ -43,6 +98,34 @@ describe('firefox tests', () => {
       JSON.parse(`
 			{
 			  "manifest_version": 2
+			}
+      `)
+    );
+  });
+
+  it('nested vendor keys', () => {
+    expect(
+      transformManifest(
+        JSON.parse(`
+			{
+			 "options_ui": {
+				"page": "options.html",
+				"open_in_tab": true,
+				"__chrome__chrome_style": false,
+				"__firefox|opera__browser_style": false
+			  }
+			}
+        `),
+        Browser.FIREFOX
+      )
+    ).toEqual(
+      JSON.parse(`
+			{
+			 "options_ui": {
+				"page": "options.html",
+				"open_in_tab": true,
+				"browser_style": false
+			  }
 			}
       `)
     );
